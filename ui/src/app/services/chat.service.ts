dd735ad5094ae7453390dd23fc8783013e17fe15
Johan Himely Mendoza <54819815+jhimelymendoza@users.noreply.github.com>
2026-05-02 21:51:14 -0300

    fix(ui): update imports

--- ui/src/app/services/chat.service.ts
+++ ui/src/app/services/chat.service.ts
@@ -1,14 +1,14 @@
-import { HttpClient } from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
 import {inject, Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
-import { IChat } from '../dto/chat.interface';
-import { environment } from '../../../environments/environment';
+import {IChat} from '../dto/chat.interface';
+import {environment} from '../../environments/environment';
 
 @Injectable({providedIn: 'root'})
 export class ChatService {
-  httpClient=inject(HttpClient);
+  httpClient = inject(HttpClient);
 
-  ask(question:string):Observable<IChat>{
+  ask(question: string): Observable<IChat> {
     return this.httpClient.get<IChat>(`${environment.apiUrl}/ask?prompt=${question}`);
   }
 }
--- ui/src/app/services/timeline.service.ts
+++ ui/src/app/services/timeline.service.ts
@@ -1,14 +1,15 @@
-import { HttpClient } from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
 import {inject, Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
-import { TimelineEntry } from '../dto/timeline.interface';
-import { environment } from '../../../environments/environment';
+import {TimelineEntry} from '../dto/timeline.interface';
+import {environment} from '../../environments/environment';
+
 
 @Injectable({providedIn: 'root'})
 export class TimelineService {
-  httpClient=inject(HttpClient);
+  httpClient = inject(HttpClient);
 
-  getProjects():Observable<TimelineEntry[]>{
+  getProjects(): Observable<TimelineEntry[]> {
     return this.httpClient.get<TimelineEntry[]>(`${environment.apiUrl}/projects`);
   }
 }
